c3e5f38fbce813aa2f165d2ac7276c34c29b749a
Danil Kolosov <kolosovdanil32@gmail.com>
2024-06-22 23:19:44 +0300

    edited ver. 22.06.24 - the main page was slightly redesigned, contact pages were created, registration for excursions, vacancies, pages for cars also changed.
    Co-Authored-By: BohdanRubanov <110721603+BohdanRubanov@users.noreply.github.com>
    Co-Authored-By: RinatUA <116442734+RinatUA@users.noreply.github.com>

--- japtune/contact_us/forms.py
+++ japtune/contact_us/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label="Ім'я")
-    email = forms.EmailField(label='Ел. пошта')
-    message = forms.CharField(widget=forms.Textarea, label='Ваш текст')
+    name = forms.CharField(max_length=100, label="Ім'я", widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+    email = forms.EmailField(label='Ел. пошта', widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+    message = forms.CharField(label='Ваш текст', widget=forms.Textarea(attrs={'class': 'form-control mb-3'}))
--- japtune/excursions/forms.py
+++ japtune/excursions/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ExcursionsForm(forms.Form):
-    name = forms.CharField(max_length=100, label="Ім'я")
-    email = forms.EmailField(label='Ел. пошта')
-    phone = forms.CharField(widget=forms.Textarea, label='Ваш номер телефону')
+    name = forms.CharField(max_length=100, label="Ім'я", widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+    email = forms.EmailField(label='Ел. пошта', widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+    phone = forms.CharField(label='Ваш номер телефону', widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
